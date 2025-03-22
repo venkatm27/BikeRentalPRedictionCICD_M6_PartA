bff67659b6ed1b24e22fdb1d24274b81f24c7a8c
venkatm27 <venkateswara3107@gmail.com>
2025-03-22 11:58:12 +0530

    Update __init__.py

--- bikeshare_project/bikeshare_model/__init__.py
+++ bikeshare_project/bikeshare_model/__init__.py
@@ -5,5 +5,5 @@ parent, root = file.parent, file.parents[1]
 sys.path.append(str(root))
 
 from bikeshare_model.config.core import PACKAGE_ROOT, config
-with open(PACKAGE_ROOT / "VERSION") as version_file:
+with open(PACKAGE_ROOT / "VERSION",encoding="utf-8") as version_file:
     __version__ = version_file.read().strip()
