5102e57181f4b1ac42270bc774df63918a203f6b
venkatm27 <venkateswara3107@gmail.com>
2025-03-22 11:59:13 +0530

    Update train_pipeline.py

--- bikeshare_project/bikeshare_model/train_pipeline.py
+++ bikeshare_project/bikeshare_model/train_pipeline.py
@@ -4,7 +4,7 @@ file = Path(__file__).resolve()
 parent, root = file.parent, file.parents[1]
 sys.path.append(str(root))
 
-import pandas as pd
+#import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score
 
